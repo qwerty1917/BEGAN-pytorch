7dabaf420b2efe3aeb0acd19af25ff18f43f90c5
H.Park <hyeongminpark5@gmail.com>
2019-05-02 22:43:20 +0900

    tmp commented

--- solver.py
+++ solver.py
@@ -202,12 +202,12 @@ class BEGAN(object):
 
             for idx, (images, labels) in enumerate(self.data_loader['train']):
                 ######## test block ########
-                tmp_sample_img = images[0]
-                print(tmp_sample_img.size())
-                print("mean: {}".format(torch.mean(tmp_sample_img)))
-                print("max: {}".format(torch.max(tmp_sample_img)))
-                print("min: {}".format(torch.min(tmp_sample_img)))
-                print("std: {}".format(tmp_sample_img.std()))
+                # tmp_sample_img = images[0]
+                # print(tmp_sample_img.size())
+                # print("mean: {}".format(torch.mean(tmp_sample_img)))
+                # print("max: {}".format(torch.max(tmp_sample_img)))
+                # print("min: {}".format(torch.min(tmp_sample_img)))
+                # print("std: {}".format(tmp_sample_img.std()))
                 ######## test block ########
                 self.global_iter += 1
 
