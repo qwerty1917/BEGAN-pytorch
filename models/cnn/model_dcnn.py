02ab7cf2df63d59bc8c7c33fb46751b1bf9a4fdc
H.Park <hyeongminpark5@gmail.com>
2019-05-29 15:42:01 +0900

    debug

--- models/cnn/model_dcnn.py
+++ models/cnn/model_dcnn.py
@@ -14,21 +14,21 @@ class Dcnn(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             # State (16x64x64)
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
 
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             # State (32x32x32)
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
 
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             # State (64x16x16)
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
 
