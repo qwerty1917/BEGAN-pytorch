31365bb1a8c6067e00db66b1b7425d9f2962ed88
H.Park <hyeongminpark5@gmail.com>
2019-05-29 18:46:16 +0900

    debug

--- solvers/solver_dcnn.py
+++ solvers/solver_dcnn.py
@@ -212,7 +212,7 @@ class DCNN(object):
         eval_acc = 100 * correct / total
         test_loss = self.criterion(outputs, labels)
         env_name = self.args.env_name
-        print("Env name: {}".format(env_name))
+        print("##### Env name: {} #####".format(env_name))
         print("Epoch: {}, iter: {}, test loss: {:.4f}, Test acc.: {:.4f}".format(self.epoch_i, self.global_iter, test_loss, eval_acc))
 
 
