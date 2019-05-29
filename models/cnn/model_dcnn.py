ae4481075d6f8b57cc43e7aa71d182e66eb8747c
H.Park <hyeongminpark5@gmail.com>
2019-05-29 15:56:22 +0900

    debug

--- models/cnn/model_dcnn.py
+++ models/cnn/model_dcnn.py
@@ -60,7 +60,7 @@ class Dcnn(nn.Module):
             # State (1024x1x1)
             nn.Linear(1024, 128),
             nn.ReLU(),
-            # nn.Dropout(0.4),
+            nn.Dropout(0.4),
 
             # # State (128)
             # nn.Linear(128, 128),
