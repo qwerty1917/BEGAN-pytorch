56dd2323f795732625bb6296f643e08dd434822b
H.Park <hyeongminpark5@gmail.com>
2019-06-05 16:54:06 +0900

    comment delete

--- models/wgan/model_residual.py
+++ models/wgan/model_residual.py
@@ -68,48 +68,6 @@ class Discriminator(nn.Module):
         return o
 
 
-# class Generator(nn.Module):
-#     def __init__(self, input_channel):
-#         super().__init__()
-#         # Input_dim = 100
-#         # Output_dim = (Cx128x128)
-#         self.main_module = nn.Sequential(
-#             # Z latent vector 100
-#             nn.ConvTranspose2d(in_channels=100, out_channels=128, kernel_size=4, stride=1, padding=0),
-#             nn.BatchNorm2d(num_features=128),
-#             nn.ReLU(True),
-#
-#             # State (1024x4x4)
-#             nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1),
-#             nn.BatchNorm2d(num_features=64),
-#             nn.ReLU(True),
-#
-#             # State (512x8x8)
-#             nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1),
-#             nn.BatchNorm2d(num_features=32),
-#             nn.ReLU(True),
-#
-#             # State (256x16x16)
-#             nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=2, padding=1),
-#             nn.BatchNorm2d(num_features=16),
-#             nn.ReLU(True),
-#
-#             # State (128x32x32)
-#             nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=4, stride=2, padding=1),
-#             nn.BatchNorm2d(num_features=8),
-#             nn.ReLU(True),
-#
-#             # State (64x64x64)
-#             nn.ConvTranspose2d(in_channels=8, out_channels=input_channel, kernel_size=4, stride=2, padding=1))
-#         # State (1x128x128)
-#
-#         self.output = nn.Tanh()
-#
-#     def forward(self, x):
-#         x = self.main_module(x)
-#         return self.output(x)
-
-
 class Generator(nn.Module):
     def __init__(self, input_channel):
         super().__init__()
