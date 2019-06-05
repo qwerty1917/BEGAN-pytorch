172fb32099cd79c7318ea1655d5faab3de3fa07e
H.Park <hyeongminpark5@gmail.com>
2019-06-05 15:21:14 +0900

    debug

--- models/wgan/model_residual.py
+++ models/wgan/model_residual.py
@@ -75,32 +75,32 @@ class Generator(nn.Module):
         # Output_dim = (Cx128x128)
         self.main_module = nn.Sequential(
             # Z latent vector 100
-            nn.ConvTranspose2d(in_channels=100, out_channels=64, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm2d(num_features=64),
+            nn.ConvTranspose2d(in_channels=100, out_channels=128, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(num_features=128),
             nn.ReLU(True),
 
             # State (1024x4x4)
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(True),
+
+            # State (512x8x8)
             nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(True),
 
-            # State (512x8x8)
+            # State (256x16x16)
             nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(True),
 
-            # State (256x16x16)
+            # State (128x32x32)
             nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=8),
             nn.ReLU(True),
 
-            # State (128x32x32)
-            nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=4),
-            nn.ReLU(True),
-
             # State (64x64x64)
-            nn.ConvTranspose2d(in_channels=4, out_channels=input_channel, kernel_size=4, stride=2, padding=1))
+            nn.ConvTranspose2d(in_channels=8, out_channels=input_channel, kernel_size=4, stride=2, padding=1))
         # State (1x128x128)
 
         self.output = nn.Tanh()
